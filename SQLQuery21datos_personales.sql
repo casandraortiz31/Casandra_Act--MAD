811ad2f03fd0306cf7473843a7b94fc36c5e36ca
casandraortiz31 <141098822+casandraortiz31@users.noreply.github.com>
2025-06-18 22:08:49 -0600

    Add files via upload

---
+++ SQLQuery21datos_personales.sql
@@ -0,0 +1,14 @@
+insert into datos_personales(ID,Nombre,Direccion,Ciudad,Num_telefono,Email,Genero)
+
+values
+
+('1','Sonia Alejandra Fernandez Moreno','Calle Roble#507Fracc.Las Mercedes','San Luis Potosi',123-456-7890,'Sonia@example.com','Femenino'),
+('2','Maria Balvina','Calle Dr.Jesús Díaz de León #438 col. Obraje','Aguascalientes',674-684-9950,'MariaBalvina@example.com','Femenino'),
+('3','Carolina','Calle Dr.Obrador Díaz','Mazatlan',614-390-6721,'Carolina@example.com','Masculino'),
+('4','Merari','Av.de la Marina#6204,Marina,local 35','Zapopan',618-196-2954,'Merari@example.com','Femenino'),
+('5','Eduardo','Av.Manuela J. Clouthier 525 col. Benito Juarez','Zacatecas',492-930-1250,'Eduardo@example.com','Masculino'),
+('6','Mario armando','Av.Melchor Ocampo#2528 Zona Centro','Durango',352-764-3572,'Mario@example.com','Masculino'),
+('7','Mauro','Calle Constitucion #106 Zona Centro','Chihuahua',333-784-1230,'Mauro@example.com','Masculino'),
+('8','Ricardo','Av.Hidalgo#338 Zacatecas Centro','Zacatecas',492-930-1250,'Ricardo@example.com','Masculino'),
+('9','Raquel','Av.Cuahtemoc Salazar#45','Aguascalientes',875-673-8545,'Raquel@example.com','Femenino'),
+('10','Karla','Av.Rios de los Remedios','San Luis Potosi',756-234-7840,'Karla@example.com','Femenino')
