435423dc50bd97744dd9613641fe859e62fc535e
casandraortiz31 <141098822+casandraortiz31@users.noreply.github.com>
2025-06-18 22:08:27 -0600

    Add files via upload

---
+++ SQLQuery20datos_sucursal.sql
@@ -0,0 +1,14 @@
+insert into sucursal(ID,Nombre_sucursal,Ciudad,Cantidad_ventas_anual)
+
+values
+
+('111','La mercedes','San Luis Potosi',96878),
+('112','Obraje','Estado de Mexico',40687),
+('113','Zapopan','Aguascalientes',7895446),
+('114','Galerías Mazatlán','Mazatlan',97687),
+('115','Constitucion','Zapopan',6545465),
+('116','Melchor','Chihuahua',10354787),
+('117', 'Centro','cuidad de Mexico',198435),
+('118','Coacalco','Durango',72179990),
+('119','Zaragosa','Zacatecas',96887998),
+('110', 'Indios Verdes','Yucatan',1338980)
