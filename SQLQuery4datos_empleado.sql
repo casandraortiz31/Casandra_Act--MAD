c30bdcc6bd2f41cc39e8da43a89447889d1ac07a
casandraortiz31 <141098822+casandraortiz31@users.noreply.github.com>
2025-06-18 22:09:06 -0600

    Add files via upload

---
+++ SQLQuery4datos_empleado.sql
@@ -0,0 +1,14 @@
+insert into datos_empleado(Codigo,FK_datos_personales,FK_sucursal,Cargo,Horario,Salario)
+
+values
+
+('11','2','21','Gerente de Area','6:00',4500),
+('12','4','24','Jefe de Area','4:00',5000),
+('13','6','26','Ingenieros','2:00',5500),
+('14','8','28','Diseño Grafico','10:00',6000),
+('15','1','21','Tecnico de Soporte','3:30',6500),
+('16','3','23','Jefe de Almacen','7:50',7000),
+('17','5','25','Flejador','9:45',7500),
+('18','7','27','Surtidor','8:00',8000),
+('19','9','29','Asistente de Sistemas','5:10',8500),
+('20','10','30','Oficina Contro','12:45',9000)
